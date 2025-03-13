bb03b71d36b3e1d5a7015285b13878e7d0418d24
rayvieira307 <vieirarayssa167@gmail.com>
2025-03-13 12:37:08 -0300

    Aprendendo

---
+++ src/main/java/me/dio/curso_dio/CursoDioApplication.java
@@ -0,0 +1,13 @@
+package me.dio.curso_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/curso_dio/CursoDioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.curso_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
