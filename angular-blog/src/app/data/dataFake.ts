9f4540edd862a9a94502cc792ecbee19b1d097ea
rayvieira307 <vieirarayssa167@gmail.com>
2025-03-26 19:22:06 -0300

    Blog com Angular

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "BLOG SOBRE MEUS PROJETOS",
+    "description": "Olá, meu nome e Rayssa, uma desenvolvedora apaixonado por tecnologia e design. Trabalho com desenvolvimento web e estou sempre em busca de novos desafios para aprimorar minhas habilidades.",
+    "photoCover":"https://avatars.githubusercontent.com/u/132487508?v=4"
+  },
+  {
+    "id":"2",
+    "title": "Gerenciador de Eventos",
+    "description": "Feito com React, Java e Postgree",
+    "photoCover":""
+  },
+
+  {
+    "id":"3",
+    "title": "Simulador de Compras",
+    "description": "Feito com C#, Angular, Sqlite e PowerBi para uma melhor visualizacao de dados.",
+    "photoCover":""
+  },
+
+  {
+    "id":"4",
+    "title": "Petropolis Participa",
+    "description": "Feito em grupo durante minha residencia, utilizanos React, Java, PostgreSql e Acessibilidade ",
+    "photoCover":""
+  }
+
+]
